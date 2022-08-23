697afe2f81c063b60cfac2ff64bb4a078189f27a
pedrocanddz <pedrocanddz07@gmail.com>
2022-08-23 13:58:20 -0300

    testes

--- src/gerenciador/Banco.java
+++ src/gerenciador/Banco.java
@@ -7,14 +7,17 @@ public class Banco {
   }
 
   // Função que retira saldo do jogador.
-  public void cobrarJogador(Jogador j, int valor) {
-    if (j.remSaldo(valor) == false); // Jogador entrou em falencia com o banco.
-
+  public boolean cobrarJogador(Jogador j, int valor) {
+    if (j.remSaldo(valor)){
+        return true;
+    } 
+    // Jogador entrou em falencia com o banco.
+    return false;    
     // j.removerPropriedades()
   }
 
   public void intermediar(Jogador j1, Jogador j2, int valor) {
-    if (j1.remSaldo(valor) == false) { // Jogador 1 entrou em falencia devido a outro jogador.
+    if (!j1.remSaldo(valor)) { // Jogador 1 entrou em falencia devido a outro jogador.
       j2.addSaldo(j1.getSaldo()); // Jogador 2 recebe todo seu saldo.
       j1.remSaldo(j1.getSaldo()); // Saldo do jogador é zerado.
 
--- src/gerenciador/Gerenciador.java
+++ src/gerenciador/Gerenciador.java
@@ -10,29 +10,29 @@ public class Gerenciador {
     public static void main(String[] args) {
 
         // Teste da classe de dados
-//        Dado d1 = new Dado();
-//        Dado d2 = new Dado();
-//        for (int i = 0; i < 8; i++)
-//            System.out.println(d1.rolar() + " " + d2.rolar());
-
+        Dado d1 = new Dado();
+        Dado d2 = new Dado();
+        for (int i = 0; i < 8; i++)
+            System.out.println(d1.rolar() + " " + d2.rolar());
+        // Fim teste da classe Dados.
         // Teste da classe decks de cartas e cartas
         DeckDeCartas cofre = new DeckDeCartas("Cofre");
             
-//        for (int i = 0; i < 18; i++) {
-//            Carta c = cofre.puxarCarta();
-//            System.out.println(c.getTipoCarta() + " " + c.getDescricao() + " " + c.getTipo() + " " + c.getQtd());
-//        }
-
+        for (int i = 0; i < 18; i++) {
+            Carta c = cofre.puxarCarta();
+            System.out.println(c.getTipoCarta() + " " + c.getDescricao() + " " + c.getTipo() + " " + c.getQtd());
+        }
+        // Fim teste Classe DeckDeCartas
 
         // Teste classe Tabuleiro
         Tabuleiro t = new Tabuleiro();
-//        for(int i = 0; i < t.getEspacos(); i++)
-//            System.out.println(t.espacos[i].getNome());
-        
-
+        for(int i = 0; i < t.getEspacos(); i++)
+            System.out.println(t.espacos[i].getNome());
+//         Fim teste classe Tabuleiro
+    
         // Testes classe Jogador
+        // Teste 1
         Jogador j = new Jogador("Player 1", t);
-        //System.out.println("Nome: " + j.getNome() + "\nSaldo: "+ j.getSaldo());
         System.out.println("Saldo:" + j.getSaldo());
         if(j.remSaldo(1600)){
             System.out.println("Removido 1600 reais");
@@ -46,23 +46,27 @@ public class Gerenciador {
         }else{
             System.out.println("Não posso remover 1600 reais");
         }
-        
         System.out.println("Novo saldo: " + j.getSaldo());
-//        j.addSaldo(-100);
-//        System.out.println(j.getSaldo());
-//        
-//        if(!j.getPreso()){
-//            System.out.println("Jogador não está preso");
-//        }else{
-//            System.out.println("Jogador está preso");
-//        }
-//        j.setPreso(true);
-//        if(!j.getPreso()){
-//            System.out.println("Jogador não está preso");
-//        }else{
-//            System.out.println("Jogador está preso");
-//        }
-//        System.out.println("Jogador está inicialmente em: " + j.getEspacoAtual());
+        
+        // Teste 2
+        System.out.println("Saldo antes da adição: " + j.getSaldo());
+        j.addSaldo(-100);
+        System.out.println("Saldo depois: " + j.getSaldo());
+        
+        // Teste 3
+        if(!j.getPreso()){
+            System.out.println("Jogador não está preso");
+        }else{
+            System.out.println("Jogador está preso");
+        }
+        System.out.println("Colocando o jogador preso");
+        j.setPreso(true);
+        if(!j.getPreso()){
+            System.out.println("Jogador não está preso");
+        }else{
+            System.out.println("Jogador está preso");
+        }
+        // Fim testes da classe Jogador
 //        
     }
 
